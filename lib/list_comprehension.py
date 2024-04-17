c9138622106dfe5b84ebf69f2af7e4b081c4be62
Genesis McGruder <genesismcgruder@gmail.com>
2024-04-16 20:17:52 -0400

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_nums = []
+    for num in num_list:
+        if num%2 == 0:
+            even_nums.append(num)
+    return even_nums
+
 
 def make_exclamation(sentence_list):
-    pass
+    new_sentence = [sentence + "!" for sentence in sentence_list]
+    return new_sentence
